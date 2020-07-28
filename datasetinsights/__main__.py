e4f9d78429c23f343c3be20993432398211a723b
Saurav Dhakad <saurav.dhakad@saurav.dhakad-2Y8M>
2020-07-27 20:59:01 -0700

    updated skeleton code

--- datasetinsights/__main__.py
+++ datasetinsights/__main__.py
@@ -1,8 +1,8 @@
-import click
 import argparse
 import logging
 import sys
 
+import click
 import torch
 from tensorboardX import SummaryWriter
 from yacs.config import CfgNode as CN
@@ -29,117 +29,156 @@ logger = logging.getLogger(__name__)
 
 @click.group()
 @click.option(
-    "--local_rank",
-    help="the local rank of the subprocess excecuting the code. "
-    "If sequential, this should be 0."
-    "Otherwise torch.distributed.launch will specify this arg.",
-    type=int,
-)
-@click.option(
     "-v",
     "--verbose",
+    is_flag=True,
     default=system.verbose,
-    help="turn on verbose mode to enable debug logging",
+    help="Turn on verbose mode to enable debug logging.",
 )
 @click.option(
-    "--dryrun",
+    "--dry-run",
+    is_flag=True,
     default=system.dryrun,
-    help="dry run with only a small subset of the dataset",
+    help="Dry run with only a small subset of the dataset.",
+)
+@click.pass_context
+def cli(ctx, verbose, dry_run):
+    """Datasetinsights Interface"""
+    ctx.ensure_object(dict)
+    ctx.obj["verbose"] = verbose
+    ctx.obj["dry_run"] = dry_run
+
+
+# Train sub-command
+@cli.command(
+    short_help="Start Training",
+    context_settings=dict(ignore_unknown_options=True, allow_extra_args=True),
 )
 @click.option(
-    "--logdir",
+    "--gpus-per-node",
+    default=get_world_size(),
+    help="Number of GPUs per node to use for training.",
+)
+@click.option(
+    "--no-cuda/--cuda",
+    default=system.no_cuda,
+    help="Force to turn off CUDA training.",
+)
+@click.option(
+    "--log-dir",
     type=str,
     default=system.logdir,
-    help="path where to save training logs",
+    help="Path for saving training logs.",
+)
+@click.option(
+    "--metrics-dir",
+    type=str,
+    default=system.metricsdir,
+    help="Path for saving metrics generated during training.",
+)
+@click.option(
+    "--metrics-filename",
+    type=str,
+    default=system.metricsfilename,
+    help="File name of metrics generated.",
+)
+@click.option(
+    "-c",
+    "--config",
+    type=str,
+    required=True,
+    help="Config file for this " "model.",
+)
+@click.pass_context
+def train(
+    ctx, gpus_per_node, no_cuda, log_dir, metrics_dir, metrics_filename, config
+):
+    verbose = ctx.obj["verbose"]
+    dry_run = ctx.obj["dry_run"]
+    if verbose:
+        click.echo("Training Started with Verbose")
+    else:
+        click.echo("Training Started")
+    click.echo("Unknown Args: %s" % ctx.args)
+
+
+# Evaluate sub-command
+@cli.command(
+    short_help="Start Evaluation",
+    context_settings=dict(ignore_unknown_options=True, allow_extra_args=True),
+)
+@click.option(
+    "--gpus-per-node",
+    type=int,
+    default=get_world_size(),
+    help="Number of GPUs per node to use for evaluation.",
+)
+@click.option(
+    "--no-cuda/--cuda",
+    default=system.no_cuda,
+    help="Force to turn off CUDA training.",
+)
+@click.option(
+    "--log-dir",
+    type=str,
+    default=system.logdir,
+    help="Path for saving evaluation logs",
+)
+@click.option(
+    "--metrics-dir",
+    type=str,
+    default=system.metricsdir,
+    help="Path for saving metrics generated during evaluation.",
+)
+@click.option(
+    "--metrics-filename",
+    type=str,
+    default=system.metricsfilename,
+    help="File name of metrics generated.",
+)
+@click.option(
+    "-c",
+    "--config",
+    type=str,
+    required=True,
+    help="Config file for this " "model.",
+)
+@click.pass_context
+def evaluate(
+    ctx, gpus_per_node, no_cuda, log_dir, metrics_dir, metrics_filename, config
+):
+    verbose = ctx.obj["verbose"]
+    dry_run = ctx.obj["dry_run"]
+    click.echo("Evaluation Started")
+
+
+# Download sub-command
+@cli.command(
+    short_help="Start Download",
+    context_settings=dict(ignore_unknown_options=True, allow_extra_args=True),
+)
+@click.option(
+    "--auth-token",
+    type=str,
+    default=system.auth_token,
+    help="Authorization token to fetch USim manifest file that specified "
+    "signed URLs for the simulation dataset files.",
 )
 @click.option(
     "--data-root",
     type=str,
     default=system.data_root,
-    help="root directory of all datasets",
+    help="Root directory of all dataset",
 )
 @click.option(
-    "--val-interval",
-    type=int,
-    default=system.val_interval,
-    help="control how many training epochs between each validation run",
+    "--log-dir", type=str, default=system.logdir, help="Path for saving logs",
 )
-@click.argument(
-    "-c", "--config", type=str, help="config file for this model",
-)
-@click.option(
-    "-j",
-    "--workers",
-    type=int,
-    default=system.workers,
-    help="number of data loading workers",
-)
-def main():
-    pass
+@click.pass_context
+def download(ctx, log_dir, auth_token, data_root):
+    verbose = ctx.obj["verbose"]
+    dry_run = ctx.obj["dry_run"]
+    click.echo("Downloading Data")
 
 
-@main.command()
-@click.option(
-    "--auth-token",
-    type=str,
-    default=system.auth_token,
-    help=(
-        "authorization token to fetch USim manifest file that speficed "
-        "signed URLs for the simulation dataset files."
-    ),
-)
-def download():
-    pass
-
-
-@main.command()
-@click.option(
-    "--gpus-per-node",
-    default=get_world_size(),
-    help="number of gpus per node to use for training/evaluating",
-)
-@click.option(
-    "--no-cuda", default=system.no_cuda, help="force to turn off cuda training",
-)
-@click.option(
-    "--metricsdir",
-    type=str,
-    default=system.metricsdir,
-    help="path where to save metrics",
-)
-@click.option(
-    "--metricsfilename",
-    type=str,
-    default=system.metricsfilename,
-    help="path where to save metrics",
-)
-def train():
-    pass
-
-
-@main.command()
-@click.option(
-    "--gpus-per-node",
-    default=get_world_size(),
-    help="number of gpus per node to use for training/evaluating",
-)
-@click.option(
-    "--no-cuda", default=system.no_cuda, help="force to turn off cuda training",
-)
-@click.option(
-    "--metricsdir",
-    type=str,
-    default=system.metricsdir,
-    help="path where to save metrics",
-)
-@click.option(
-    "--metricsfilename",
-    type=str,
-    default=system.metricsfilename,
-    help="path where to save metrics",
-)
-def evaluate():
-    pass
-
-
-main()
+# cli call
+cli(prog_name="python -m datasetinsights", obj={})
