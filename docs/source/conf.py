e24ed3c7f0ca92ac31e118d32538f8508ec4365d
adamp <adamp@unity3d.com>
2020-07-28 15:35:41 +0200

    Adding autoapi.extension to reduce memory footprint of build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,12 +34,17 @@ extensions = [
     "sphinx.ext.autosectionlabel",
     "sphinx_rtd_theme",
     "sphinx.ext.napoleon",
+    "autoapi.extension"
 ]
+
+autoapi_dirs = ['../../datasetinsights']
+
 source_suffix = {
     ".rst": "restructuredtext",
     ".txt": "markdown",
     ".md": "markdown",
 }
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
