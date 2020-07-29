dbd6eea859417574022ad8266d7b0c367d7f7de5
adamp <adamp@unity3d.com>
2020-07-29 16:55:04 +0200

    Removed empty line

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,6 @@ napoleon_google_docstring = True
 master_doc = 'index'
 
 
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
