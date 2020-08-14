c53bf295a727050a9bf06074e7a52cbb38d0ead6
“sanjay” <sanjay.vishwakarma@unity3d.com>
2020-08-14 14:28:51 -0700

    import fix

--- datasetinsights/datasets/__init__.py
+++ datasetinsights/datasets/__init__.py
@@ -3,7 +3,6 @@ from .cityscapes import Cityscapes
 from .coco import CocoDetection
 from .groceries_real import GoogleGroceriesReal, GroceriesReal
 from .nyudepth import NyuDepth
-from .synthetic import SynDetection2D, read_bounding_box_2d
 
 __all__ = [
     "Cityscapes",
@@ -12,6 +11,4 @@ __all__ = [
     "GroceriesReal",
     "GoogleGroceriesReal",
     "NyuDepth",
-    "SynDetection2D",
-    "read_bounding_box_2d",
 ]
