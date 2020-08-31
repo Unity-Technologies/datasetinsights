95dc58b62fc1be352c1ee57d97e2e7fc25f95e52
adamp <adamp@unity3d.com>
2020-08-31 18:52:55 +0200

    Adding test_create_estimator.py that test creation of FasterRCNN.

---
+++ tests/test_create_estimator.py
@@ -0,0 +1,24 @@
+import pytest
+from yacs.config import CfgNode as CN
+
+from datasetinsights.estimators import create_estimator
+from datasetinsights.estimators import FasterRCNN
+
+
+@pytest.fixture
+def config():
+    """prepare config."""
+    with open("tests/configs/faster_rcnn_groceries_real_test.yaml") as f:
+        cfg = CN.load_cfg(f)
+
+    return cfg
+
+
+def test_create_estimator_with_config_expect_faster_rcnn(config):
+
+    estimator = create_estimator(
+        name=config.estimator,
+        config=config,
+    )
+
+    assert isinstance(estimator, FasterRCNN)
