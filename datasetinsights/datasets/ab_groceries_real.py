5729ea153bf2a73ffa80a3586aac48d4ab5660c1
Jon Hogins <jonathanh@unity3d.com>
2020-09-11 23:57:27 -0700

    Fixing bugs with ABGroceriesReal dataset loader and ab test config

--- datasetinsights/datasets/ab_groceries_real.py
+++ datasetinsights/datasets/ab_groceries_real.py
@@ -1,21 +1,35 @@
 
+from .base import Dataset
 from .groceries_real import GroceriesReal
 import datasetinsights.constants as const
 from ..io import BBox2D
 
-
-class ABGroceriesReal(GroceriesReal):
+class ABGroceriesReal(Dataset):
 
     def __init__(self, *, data_path=const.DEFAULT_DATA_ROOT, split="train", transforms=None, version="v3", target_label=None, **kwargs):
+        self.groceries_real = GroceriesReal(
+            data_path=data_path, split=split, transforms=None, version=version, **kwargs
+        )
+        self.transforms = transforms
         self.target_label = target_label
-        super().__init__(data_path=data_path, split=split, transforms=transforms, version=version, **kwargs)
-        self.target_id = next((id for id, label in self.label_mappings.items() if label == target_label), None)
+        self.target_id = next((id for id, label in self.groceries_real.label_mappings.items() if label == target_label), None)
         if self.target_id is None:
-            raise ValueError(f"No label {target_label} in dataset. Labels are: {self.label_mappings}")
+            raise ValueError(f"No label {target_label} in dataset. Labels are: {self.groceries_real.label_mappings}")
 
         self.label_mappings = {1: target_label}
 
     def __getitem__(self, idx):
-        image, bboxes = super().__getitem__(idx)
+        image, bboxes = self.groceries_real.__getitem__(idx)
+        for bbox in bboxes:
+            if isinstance(bbox, str):
+                raise ValueError(f"bbox for index {idx} is a string: {bbox}")
+
         bboxes = [BBox2D(x=bbox.x, y=bbox.y, w=bbox.w, h=bbox.h, label=1) for bbox in bboxes if bbox.label == self.target_id ]
+
+        if self.transforms:
+            image, bboxes = self.transforms(image, bboxes)
+
         return image, bboxes
+
+    def __len__(self):
+        return self.groceries_real.__len__()
