40aad954fb0c5dd29ec5338b833986e4bb183e7e
Bowen Li <bowen.li@unity3d.com>
2021-01-05 16:05:18 -0800

    add pr curve

--- datasetinsights/evaluation_metrics/__init__.py
+++ datasetinsights/evaluation_metrics/__init__.py
@@ -9,6 +9,7 @@ from .average_recall_2d import AverageRecall, MeanAverageRecallAverageOverIOU
 from .average_relative_error import AverageRelativeError
 from .base import EvaluationMetric
 from .iou import IoU
+from .pr_curve import PRCurve
 from .root_mean_square_error import RootMeanSquareError
 from .threshold_accuracy import ThresholdAccuracy
 
@@ -23,6 +24,7 @@ __all__ = [
     "MeanAveragePrecisionAverageOverIOU",
     "MeanAveragePrecisionIOU50",
     "MeanAverageRecallAverageOverIOU",
+    "PRCurve",
     "RootMeanSquareError",
     "ThresholdAccuracy",
 ]
