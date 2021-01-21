6525600a17a6354f33bdfc614b06f996d965ea46
“sanjay” <sanjay.vishwakarma@unity3d.com>
2021-01-21 15:18:57 -0800

    rebase and added prepare config

--- tests/test_config_handler.py
+++ tests/test_config_handler.py
@@ -1,8 +1,38 @@
 from unittest.mock import MagicMock, patch
 
 import pytest
+from yacs.config import CfgNode as CN
 
-from datasetinsights.io.config_handler import load_config
+from datasetinsights.io.config_handler import (
+    load_config,
+    override_config,
+    prepare_config,
+)
+
+
+@pytest.fixture
+def config():
+    """prepare config."""
+    with open("tests/configs/faster_rcnn_groceries_real_test.yaml") as f:
+        cfg = CN.load_cfg(f)
+
+    return cfg
+
+
+def test_override_config(config):
+    actual_value = True
+    expected_value = False
+    override = f"pretrained={expected_value}"
+    config.pretrained = actual_value
+    returned_config = override_config(override=override, config=config)
+    assert returned_config["pretrained"] != actual_value
+    assert returned_config["pretrained"] == expected_value
+
+
+def test_invalid_override_key(config):
+    override = "a.b.c=test1 c.a"
+    with pytest.raises(AssertionError):
+        override_config(override=override, config=config)
 
 
 @patch("datasetinsights.io.config_handler.create_downloader")
@@ -87,3 +117,15 @@ def test_bad_path():
     bad_url = "s3://path/to/bad/url"
     with pytest.raises(ValueError):
         load_config(bad_url)
+
+
+@patch("datasetinsights.io.config_handler.override_config")
+@patch("datasetinsights.io.config_handler.load_config")
+def test_prepare_config(mock_load, mock_override):
+    mock_obj = MagicMock()
+    mock_load.return_value = mock_obj
+    config_url = "test.yaml"
+    override = "test=test"
+    prepare_config(path=config_url, override=override)
+    mock_load.assert_called_with(config_url)
+    mock_override.assert_called_with(override=override, config=mock_obj)
